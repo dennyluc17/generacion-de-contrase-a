8324188127cfef9ec719f94989046261fdce4c06
denny lucas <johandenluc14@gmail.com>
2024-12-20 01:43:08 -0500

    Add files via upload

--- contraseñas.py
+++ contraseñas.py
@@ -1,15 +1,15 @@
-import random
+
 import string
 import secrets
 
-def generador_contrseña():
+def generar_contrseña():
     #solicitar longitud de la contraseña
     longitud = int(input("ingrese la longitud de la contraseña(mínimo 8) :"))
     while longitud < 8:
         print("la longitud minima es 8. intente nuevamente.")
         longitud - int(input("ingrese la longitud deseada de la contraseña (mínimo 8)"))
 
-         # Preguntar si se desean caracteres especiales
+    # Preguntar si se desean caracteres especiales
     usar_caracteres_especiales = input("¿Desea incluir caracteres especiales? (sí/no): ").lower
     
     # Definir los conjuntos de caracteres posibles
@@ -20,3 +20,12 @@ def generador_contrseña():
         caracteres_posibles = caracteres_base + caracteres_especiales
     else:
         caracteres_posibles = caracteres_base
+
+     # Generar la contraseña aleatoria utilizando 'secrets.choice' para mayor seguridad
+    contraseña = ''.join(secrets.choice(caracteres_posibles) for _ in range(longitud))
+    
+    return contraseña
+
+# Llamada a la función para generar y mostrar la contraseña
+contraseña_generada = generar_contrseña()
+print("Contraseña generada:", contraseña_generada)
